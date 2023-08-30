4c14cffb71ededf0dadf54ac0e46b15e930e50c0
Trenton_Sallee <tretnonsmiles@gmail.com>
2023-08-30 13:21:45 -0700

    Near Completed

--- README.md
+++ README.md
@@ -1,2 +1,41 @@
 # Weather-Dashboard
-This application uses JavaScript, HTML and CSS to obtain and display the data of a third parry API.
+This application uses JavaScript, HTML and CSS to obtain and display the data of a third party weather API.
+
+## Description
+The user enters any city they wish to see a 6 day weather forcast for. The data is then fetched from Open Weather API and appended to elements displayed with Bootstrap.
+
+## Table of Contents
+* [Description](#description)
+* [Installation](#installation)
+* [Customer Prompt](#customer)
+* [Contributing](#contributing)
+* [Contact me for questions](#contact)
+
+## User Story
+
+```
+AS A traveler
+I WANT to see the weather outlook for multiple cities
+SO THAT I can plan a trip accordingly
+```
+
+## Installation
+This project does not require any instalation
+
+## Customer Prompt
+AS A traveler
+I WANT to see the weather outlook for multiple cities
+SO THAT I can plan a trip accordingly
+
+## Contributing
+* No other Collaberators
+* Third Part Assets: Open Weather API https://openweathermap.org/, Bootstrap V5.1.3
+
+## Contact
+
+GitHub
+[trentonsallee](https://github.com/trentonsallee/Weather-Dashboard)
+
+Email
+trentonsmiles@gmail.com
+
---
+++ assets/index.js
@@ -0,0 +1,102 @@
+var searchButton = $('#search-button');
+searchButton.on('click', cityInput);
+var clearHistoryEl = $('#clearHistory')
+clearHistoryEl.on('click', clearHistory)
+var historyList = JSON.parse(localStorage.getItem("storedCitiesArray")) || [];
+const apiKey = "54ab12f9f770218eab336604399c722f";
+
+function renderSavedData() {
+  var searchHistoryEl = $('#searchHistory');
+  searchHistoryEl.text('')
+  for (var i = 0; i < historyList.length; i++)
+  $('<button class="btn btn-success searchButtons m-2" type="button">').text(historyList[i]).attr("onclick", "bringBackData('" + historyList[i] + "')").appendTo(searchHistoryEl)
+};
+
+function cityInput() {
+  var citySearch = $('#citySearch').val();
+  weatherAPI(citySearch);
+  historyList.push(citySearch)
+  localStorage.setItem('storedCitiesArray', JSON.stringify(historyList));
+  renderSavedData();
+};
+function weatherAPI(city) {
+  var geocodeAPI = "https://api.openweathermap.org/geo/1.0/direct?q=" + city + "&limit=&appid=" + apiKey;
+  fetch(geocodeAPI)
+  .then(function (response) {
+    return response.json();
+  })
+  .then(function (data) {
+    var lat = data[0].lat
+    var lon = data[0].lon
+    var urlCurrent = "https://api.openweathermap.org/data/2.5/weather?lat=" + lat + "&lon=" + lon + "&appid=" + apiKey + "&units=imperial"
+    fetch(urlCurrent)
+    .then(function (response) {
+      return response.json();
+    })
+      var url = "https://api.openweathermap.org/data/2.5/forecast?lat=" + lat + "&lon=" + lon + "&appid=" + apiKey + "&units=imperial"
+      fetch(url)
+      .then(function (response) {
+        return response.json();
+      })
+      .then(function (data) {
+        $('#day0city').text(data.name)
+        var date = new Date(data.dt * 1000)
+        $('#day0date').text(date.toUTCString());
+        $('#day0icon').attr('src', 'http://openweathermap.org/img/w/' + data.weather[0].icon + '.png').attr('alt', data.weather[0].description);
+        $('#day0Desc').text(data.weather[0].description);
+        $('#day0temp').text(data.main.temp + 'F');
+        $('#day0wind').text(data.wind.speed + 'mph');
+        $('#day0humid').text(data.main.humidity + '% Humidity');
+    
+        $('#day1city').text(data.city.name)
+        var date1 = new Date(data.list[4].dt_txt)
+        $('#day1date').text(date1.toUTCString());
+        $('#day1icon').attr('src', 'http://openweathermap.org/img/w/' + data.list[4].weather[0].icon + '.png').attr('alt', data.list[8].weather[0].description);
+        $('#day1Desc').text(data.list[4].weather[0].description);
+        $('#day1temp').text(data.list[4].main.temp + 'F');
+        $('#day1wind').text(data.list[4].wind.speed + 'mph');
+
+        $('#day2city').text(data.city.name)
+        var date2 = new Date(data.list[12].dt_txt)
+        $('#day2date').text(date2.toUTCString());
+        $('#day2icon').attr('src', 'http://openweathermap.org/img/w/' + data.list[12].weather[0].icon + '.png').attr('alt', data.list[16].weather[0].description);
+        $('#day2Desc').text(data.list[12].weather[0].description);
+        $('#day2temp').text(data.list[12].main.temp + 'F');
+        $('#day2wind').text(data.list[12].wind.speed + 'mph');        $('#day3city').text(data.city.name)
+
+        var date3 = new Date(data.list[20].dt_txt)
+        $('#day3date').text(date3.toUTCString());
+        $('#day3icon').attr('src', 'http://openweathermap.org/img/w/' + data.list[20].weather[0].icon + '.png').attr('alt', data.list[24].weather[0].description);
+        $('#day3Desc').text(data.list[20].weather[0].description);
+        $('#day3temp').text(data.list[20].main.temp + 'F');
+        $('#day3wind').text(data.list[20].wind.speed + 'mph');
+
+        $('#day4city').text(data.city.name)
+        var date4 = new Date(data.list[28].dt_txt)
+        $('#day4date').text(date4.toUTCString());
+        $('#day4icon').attr('src', 'http://openweathermap.org/img/w/' + data.list[28].weather[0].icon + '.png').attr('alt', data.list[32].weather[0].description);
+        $('#day4Desc').text(data.list[28].weather[0].description);
+        $('#day4temp').text(data.list[28].main.temp + 'F');
+        $('#day4wind').text(data.list[28].wind.speed + 'mph');
+
+        $('#day5city').text(data.city.name)
+        var date5 = new Date(data.list[36].dt_txt)
+        $('#day5date').text(date5.toUTCString());
+        $('#day5icon').attr('src', 'http://openweathermap.org/img/w/' + data.list[36].weather[0].icon + '.png').attr('alt', data.list[39].weather[0].description);
+        $('#day5Desc').text(data.list[36].weather[0].description);
+        $('#day5temp').text(data.list[36].main.temp + 'F');
+        $('#day5wind').text(data.list[36].wind.speed + 'mph');    });
+  });
+};
+
+function clearHistory() {
+  localStorage.clear();
+  historyList = [];
+  renderSavedData();
+}
+
+function bringBackData(fromHistory) {
+  getWeather(fromHistory);
+}
+
+renderSavedData();
